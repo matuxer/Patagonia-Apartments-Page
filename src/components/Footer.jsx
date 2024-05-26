d9da70ff52970eb5972bd84deb8cd4e483892f42
matuxer <matias@biott.com.ar>
2024-05-25 22:54:03 -0300

    Made Footer mobile style

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import React from "react";
+import styles from "./Footer.module.css";
 
 function Footer() {
   return (
-    <div className='bg-black text-white'>Footer</div>
-  )
+    <div>
+      <div className={styles.footerBg}>
+        <div className="pt-20">
+          {/* MENSAJE */}
+          <div className="flex flex-col items-center">
+            <h2 className="font-raleway text-white font-semibold tracking-widest">
+              VISITA USHUAIA
+            </h2>
+            <h1 className="font-raleway text-white font-semibold text-4xl">
+              Contactanos!
+            </h1>
+          </div>
+          {/* DATOS */}
+          <div className="flex flex-col items-center mt-9 gap-2">
+            <div className="flex flex-row items-center">
+              <img
+                className="h-6"
+                src="/images/phone-svgrepo-com.svg"
+                alt="phone icon"
+              />
+              <p className="font-raleway text-white font-bold ml-2">
+                +54 9 2901-123456
+              </p>
+            </div>
+            <div className="flex flex-row items-center">
+              <img
+                className="h-6"
+                src="/images/mail-alt-svgrepo-com.svg"
+                alt="mail icon"
+              />
+              <p className="font-raleway text-white font-bold ml-2">
+                test@gmail.com
+              </p>
+            </div>
+          </div>
+          {/* REDES */}
+          <div className="flex flex-row gap-3 justify-center mt-9">
+            <a href="facebook.com">
+              <img className="h-9" src="/images/facebook-round-svgrepo-com.svg" alt="Facebook Icon" />
+            </a>
+            <a href="instagram.com">
+              <img className="h-9" src="/images/instagram-f-svgrepo-com.svg" alt="Instagram Icon" />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
