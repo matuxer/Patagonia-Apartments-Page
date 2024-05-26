5ad9a5cde111839c2ea8505898640defa1b79d43
matuxer <matias@biott.com.ar>
2024-05-26 00:07:01 -0300

    Added info managment to Apartments page

--- src/pages/Apartment.jsx
+++ src/pages/Apartment.jsx
@@ -1,12 +1,33 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import data from "../db.json";
 
 function Apartment() {
-    const { id } = useParams();
+  const { id } = useParams();
+  const [apartment, setApartment] = useState(null)
+  const [loading, setLoading] = useState(true)
 
-  return (
-    <div className='pt-[56px] md:pt-[80px]'>Hola Mundo! {id}</div>
-  )
+
+  useEffect(() => {
+    let info = data[0].apartments.filter((el) => {
+      return el.id === Number(id)
+    })
+    setApartment(info[0])
+    setLoading(false)
+  }, [id])
+  
+
+  if (loading) {
+    return (
+      <div id="top" className="pt-[56px] h-[700px] md:pt-[80px]">Loading</div>
+    )
+  } else {
+    return (
+      <div className="pt-[56px] h-[700px] md:pt-[80px]">
+        <img src={`/${apartment.images[0]}`} alt="" />
+      </div>
+    );
+  }
 }
 
-export default Apartment
+export default Apartment;
