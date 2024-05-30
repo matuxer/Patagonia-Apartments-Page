2f6e68eddd175a85c38842c06a1d87e5e122d9b5
matuxer <matias@biott.com.ar>
2024-05-29 21:19:30 -0300

    Added a scroll to top helper component

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import NavBar from './components/NavBar';
 import Contact from './pages/Contact';
 import Apartment from './pages/Apartment';
 import Footer from './components/Footer';
+import ScrollToTop from './helper/ScrollToTop';
 
 function App() {
   return (
     <BrowserRouter>
       <NavBar />
+      <ScrollToTop />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
