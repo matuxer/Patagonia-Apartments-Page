f6e6f4a31255003ca8326b56c840f5db61821427
matuxer <matias@biott.com.ar>
2024-06-06 16:37:08 -0300

    Changed the 404 page

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -5,14 +5,14 @@ import { Link } from "react-router-dom";
 function PageNotFound() {
   return (
     <div
-      className={`flex justify-center items-center bg-[#f0f4f4] h-[600px] pt-[56px] md:pt-[80px]`}
+      className={`flex justify-center items-center bg-[#f0f4f4] h-[80vh] pt-[56px] md:pt-[80px] ${styles.errorBg}`}
     >
       <div className="max-w-md space-y-4 text-center">
-        <h1 className="text-5xl font-bold tracking-tighter text-[#4E6E82] font-raleway md:text-6xl">
+        <h1 className="text-5xl font-bold tracking-tighter text-[#4E6E82] font-raleway md:text-7xl">
           404
         </h1>
-        <p className="text-gray-600 font-raleway md:text-xl dark:text-gray-400">
-          Oops! El apartment que estas buscando no existe.
+        <p className="text-gray-600 font-raleway font-semibold md:text-xl dark:text-gray-400">
+          Chan! Parece que el apartment que estas buscando no existe.
         </p>
         <Link
           to="/"
