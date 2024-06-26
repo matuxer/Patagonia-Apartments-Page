415aa7af7091daae80b7e10a09a439f14672af43
matuxer <matias@biott.com.ar>
2024-06-26 00:15:47 -0300

    Documented the index.js and index.css files

--- src/index.js
+++ src/index.js
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+/* Se crea la raiz del proyecto dentro del div 'root' */
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+/* Se renderiza el proyecto dentro del elemento 'root' */
 root.render(
   <React.StrictMode>
+    {/* Es llamado el elemento App */}
     <App />
   </React.StrictMode>
 );
