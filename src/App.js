011690b1d2e9dc74c109aca5d94ec538d0d0940f
matuxer <matias@biott.com.ar>
2024-06-26 00:25:05 -0300

    Documented App.js file

--- src/App.js
+++ src/App.js
@@ -13,22 +13,28 @@ import Footer from "./components/Footer";
 import ScrollToTop from "./helper/ScrollToTop";
 import PageNotFound from "./pages/PageNotFound";
 
+/* Es creada la estructura principal de las rutas del proyecto */
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<RootLayout />}>
       <Route index element={<Home />} />
       <Route path="contact" element={<Contact />} />
+
+      {/* El loader es usado para verificar si el id del apartamento dado existe o no, de existir renderiza la página Apartment, caso contrario renderiza la página PageNotFound */}
       <Route
         path="apartment/:id"
         element={<Apartment />}
         loader={apartmentLoader}
         errorElement={<PageNotFound />}
       />
+
+      {/* En caso de que el path no coincide con ninguno de los path anteriores renderiza la página PageNotFound */}
       <Route path="*" element={<PageNotFound />} />
     </Route>
   )
 );
 
+/* Se determina la distribución de ciertos componentes. Ej, componente NavBar va a ir arriba de todo y el elemento Footer va abajo de todo, y estan presentes dentro de todas las páginas */
 function RootLayout() {
   return (
     <>
@@ -40,6 +46,7 @@ function RootLayout() {
   );
 }
 
+/* Dentro del componente App se renderiza las rutas antes especificadas, mostrando la página Home en el path '/' */
 function App() {
   return <RouterProvider router={router} />;
 }
