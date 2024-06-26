68fde822c88e9ce9caeeedcb87999eda6ab7cf82
matuxer <matias@biott.com.ar>
2024-06-26 00:48:44 -0300

    Documented the ScrollToTop helper

--- src/helper/ScrollToTop.jsx
+++ src/helper/ScrollToTop.jsx
@@ -1,15 +1,15 @@
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
+// ScrollToTop es un componente helper que permite que cada página haga scroll hacia arriba cada vez que es renderizada
 export default function ScrollToTop() {
   const { pathname } = useLocation();
 
   useEffect(() => {
-    // "document.documentElement.scrollTo" is the magic for React Router Dom v6
     document.documentElement.scrollTo({
       top: 0,
       left: 0,
-      behavior: "instant", // Optional if you want to skip the scrolling animation
+      behavior: "instant",
     });
   }, [pathname]);
 
