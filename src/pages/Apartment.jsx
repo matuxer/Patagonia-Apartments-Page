5917755d6282f8a83c02cfe49ebf66739a0ebdcf
matuxer <matias@biott.com.ar>
2024-06-26 01:31:06 -0300

    Documented the Apartment page file

--- src/pages/Apartment.jsx
+++ src/pages/Apartment.jsx
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import data from "../db.json";
-import LocationIcon from "../images/location-pin-alt-1-svgrepo-com.svg";
+import Cards from "../components/Cards";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import CarouselComponent from "../components/CarouselComponent";
+import LocationIcon from "../images/location-pin-alt-1-svgrepo-com.svg";
 import RulerIcon from "../images/ruler-svgrepo-com.svg";
 import BedIcon from "../images/bed-svgrepo-com.svg";
-import Cards from "../components/Cards";
 
 function Apartment() {
   const { id } = useParams();
@@ -14,21 +14,19 @@ function Apartment() {
   const [apartments, setApartments] = useState([]);
   const [loading, setLoading] = useState(true);
 
+  /* Cuando se renderiza la página se guarda la información de los apartments */
   useEffect(() => {
     let info = data[0].apartments.filter((el) => {
       return el.id === Number(id);
     });
 
-    if (info.length > 0) {
-      setApartment(info[0]);
-      setApartments(data[0].apartments);
-      setLoading(false);
-    } else {
-      throw Error("No Apartment");
-    }
+    setApartment(info[0]);
+    setApartments(data[0].apartments);
+    setLoading(false);
   }, [id]);
 
   if (loading) {
+    /* Mientras la página esta obteniendo los datos del apartment se muestra un mensaje de carga */
     return (
       <div id="top" className="pt-[56px] h-[700px] md:pt-[80px]">
         Loading
@@ -47,6 +45,7 @@ function Apartment() {
               {apartment.location}
             </p>
 
+            {/* Información del apartment solo visible en Desktop */}
             <div className="hidden mb-6 md:block">
               <p className="font-raleway text-sm text-gray-600 font-medium flex flex-row items-center justify-start mb-3 lg:text-base">
                 <img src={RulerIcon} className="h-5 mr-2" alt="..." />
@@ -63,13 +62,13 @@ function Apartment() {
             </div>
           </div>
 
-          {/* IMAGE CAROUSEL */}
+          {/* Se llama al componente CarouselComponent el cual va a renderizar un Carrusel para mostrar las imagenes del apartment */}
           <div className="block px-3 md:pt-5 md:w-1/2">
             <CarouselComponent images={apartment.images} id={id} />
           </div>
         </div>
 
-        {/* APARTMENT INFO MOBILE */}
+        {/* Información del apartment solo visible en Mobile */}
         <div className="px-3 mb-6 sm:px-5 md:hidden">
           <p className="font-raleway text-sm text-gray-600 font-medium flex flex-row items-center justify-start mb-3">
             <img src={RulerIcon} className="h-5 mr-2" alt="..." />
@@ -85,7 +84,7 @@ function Apartment() {
           </p>
         </div>
 
-        {/* CARDS */}
+        {/* Se llama al componente Cards y se le pasa el id del apartment actual para mostrar los otros apartments salteando el actual */}
         <div className="w-full border-t-2 border-t-[#4E6E82] bg-[#f0f4f4]">
           <Cards apartments={apartments} id={Number(id)} />
         </div>
@@ -94,10 +93,12 @@ function Apartment() {
   }
 }
 
+/* Función que usa el router como loader para controlar que el id del apartment exista */
 export const apartmentLoader = async ({ params }) => {
   const id = parseInt(params.id, 10);
   const apartment = data[0].apartments.find((apartment) => apartment.id === id);
 
+  /* Si el apartment existe se retorna, caso contrario, se genera un Response de tipo 404 para avisarle al router de que no existe */
   if (apartment) {
     return apartment;
   } else {
