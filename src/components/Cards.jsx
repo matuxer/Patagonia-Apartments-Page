d1f7e22fd3ec6fb31b285f5a49196ef394460a3a
matuxer <matias@biott.com.ar>
2024-11-04 14:18:27 -0300

    Added logic for the filters to work

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,19 +1,71 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Card from "./Card";
 
+const getRandomElements = (array, numElements, excludeId) => {
+  let result = [];
+  let _array = array.filter((apartment) => apartment.id !== excludeId);
+  while (result.length < numElements && _array.length > 0) {
+    let randomIndex = Math.floor(Math.random() * _array.length);
+    result.push(_array.splice(randomIndex, 1)[0]);
+  }
+  return result;
+};
+
 /* El componente Cards recibe un array con los apartment para renderizar una Card a cada uno y un id para filtrar apartments especificos */
-function Cards({ apartments, id }) {
-  return (
-    <div className="flex flex-col justify-center items-center py-4 gap-4 md:px-4 md:gap-0 lg:flex-row lg:gap-10 lg:px-20 lg:flex-wrap ">
-      
-      {/* El método map va a iterar el array de apartments renderizando un componente Card para cada uno */}
-      {apartments?.map((el) => {
-        if (el.id !== id) {
-            return <Card props={el} key={el.id} />;
+function Cards({ apartments, id, filters }) {
+  const [filteredApartments, setFilteredApartments] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const areAllFiltersFalse = () => {
+      return Object.values(filters).every((value) => value === false);
+    };
+
+    const filterApartments = () => {
+      if (areAllFiltersFalse()) {
+        return apartments;
+      }
+
+      return apartments.filter((apartment) => {
+        if (id && apartment.id === id) {
+          return false;
         }
-      })}
-    </div>
-  );
+        for (let key in filters) {
+          if (filters[key] === true && apartment.features[key] !== true) {
+            return false;
+          }
+        }
+        return true;
+      });
+    };
+
+    if (id) {
+      setFilteredApartments(getRandomElements(apartments, 3, id));
+    } else {
+      const filtered = filterApartments();
+      setFilteredApartments(filtered);
+    }
+
+    setLoading(false);
+  }, [filters, apartments, id]);
+
+  if (loading) {
+    /* Mientras la página esta obteniendo los datos del apartment se muestra un mensaje de carga */
+    return (
+      <div id="top" className="pt-[56px] h-[700px] md:pt-[80px]">
+        Loading
+      </div>
+    );
+  } else {
+    return (
+      <div className="flex flex-col justify-center items-center py-4 gap-4 md:px-4 md:gap-0 lg:flex-row lg:gap-10 lg:px-20 lg:flex-wrap ">
+        {/* El método map va a iterar el array de apartments renderizando un componente Card para cada uno */}
+        {filteredApartments?.map((el) => {
+          return <Card props={el} key={el.id} />;
+        })}
+      </div>
+    );
+  }
 }
 
 export default Cards;
