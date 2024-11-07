223109ee9df8212b00eba2f7b4ea7a71956f61b3
matuxer <matias@biott.com.ar>
2024-11-07 10:07:34 -0300

    Commented Cards Component

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Card from "./Card";
 
+/* Funcion que randomiza los apartment que se van a mostrar segun la cantidad indicada (numElements) y que apartment excluir (excludeId) */
 const getRandomElements = (array, numElements, excludeId) => {
   let result = [];
   let _array = array.filter((apartment) => apartment.id !== excludeId);
@@ -16,20 +17,22 @@ function Cards({ apartments, id, filters }) {
   const [filteredApartments, setFilteredApartments] = useState([]);
   const [loading, setLoading] = useState(true);
 
+
+  /* El useEffect() se va a ejecutar cada vez que las variables filters, id o apartments cambien */
   useEffect(() => {
     const areAllFiltersFalse = () => {
       return Object.values(filters).every((value) => value === false);
     };
 
     const filterApartments = () => {
+
+      /* Si todos los filtros estan desactivados se devuelven todos los apartments */
       if (areAllFiltersFalse()) {
         return apartments;
       }
 
+      /* Se filtran los apartment segun cuales filtros estan en true */
       return apartments.filter((apartment) => {
-        if (id && apartment.id === id) {
-          return false;
-        }
         for (let key in filters) {
           if (filters[key] === true && apartment.features[key] !== true) {
             return false;
@@ -39,6 +42,7 @@ function Cards({ apartments, id, filters }) {
       });
     };
 
+    /* Si existe un id (página de Apartment) solo se mostrara 3 apartments aleatorios excluyendo el apartment en el que se encuentra la página en el momento. Sino, si no exite id (página de Home) se muestran todos los apartment respetando los filtros si es que hay */
     if (id) {
       setFilteredApartments(getRandomElements(apartments, 3, id));
     } else {
@@ -59,7 +63,7 @@ function Cards({ apartments, id, filters }) {
   } else {
     return (
       <div className="flex flex-col justify-center items-center py-4 gap-4 md:px-4 md:gap-0 lg:flex-row lg:gap-10 lg:px-20 lg:flex-wrap ">
-        {/* El método map va a iterar el array de apartments renderizando un componente Card para cada uno */}
+        {/* El método map va a iterar el array de filteredApartments renderizando un componente Card para cada uno */}
         {filteredApartments?.map((el) => {
           return <Card props={el} key={el.id} />;
         })}
